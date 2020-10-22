87b64a6eefe9a3a9c59250dcb16b948db0bf51c3
trichau123 <53028788+trichau123@users.noreply.github.com>
2020-10-22 11:25:07 -0400

    Create CountByFive.java
    Write an application that counts by five from 5 through 500 inclusive, and that starts a new line after every multiple of 50 (50, 100, 150, and so on).

---
+++ CountByFive.java
@@ -0,0 +1,10 @@
+public class CountByFives {
+    public static void main (String args[]) {
+        // Write your code here
+        for (int i = 5; i<= 500; i = i +5){
+            System.out.println(i);
+        }
+    }
+}
+
+
